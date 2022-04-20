bce8f80c9753b24f0d6e83ce40d107624fb9aad3
Louison <louison59279@gmail.com>
2022-04-20 16:06:47 +0200

    push après soutenance

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,11 +1,14 @@
 import styles from '../Error404/Error404.module.css'
+import { Link } from 'react-router-dom';
 
 function Error404() {
     return (
       <div className={styles.CtnError}>
         <h1 className={styles.Error404}>404</h1>
         <p className={styles.ErrorText}>Oups ! La page que vous demandez n&apos;existe pas.</p>
+        <Link to={`/`}>
         <p className={styles.ErrorRedirection}>Retourner sur la page d&apos;accueil</p>
+        </Link>
       </div>
       
     );
